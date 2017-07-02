6f8323283290a051e15721d9555264b95c49668e
manisjos <manish.joshi.d@gmail.com>
2017-07-02 20:09:01 +0530

    db changes for heroku through nodeapi wala login

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@
 module.exports = {
 
     // 'url': 'mongodb://localhost/loginApp'
-        'url': 'mongodb://manisjosH:london@1@ds145312.mlab.com:45312/loginapp_manish'
-
+        // 'url': 'mongodb://manisjosH:london@1@ds145312.mlab.com:45312/loginapp_manish'
+   'url' : 'mongodb://nodejsapi:nodejsapi@ds041516.mlab.com:41516/mynodejsapp'
 };
 	//'url' : 'your-database-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
