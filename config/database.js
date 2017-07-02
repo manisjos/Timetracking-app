400ad17be817fa66df3339239014c710d8103c9b
manisjos <manish.joshi.d@gmail.com>
2017-07-02 19:42:56 +0530

    db changes for heroku

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 //config/database.js
 module.exports = {
 
-    'url': 'mongodb://localhost/loginApp'
+    // 'url': 'mongodb://localhost/loginApp'
+        'url': 'mongodb://manisjosH:london@1@ds145312.mlab.com:45312/loginapp_manish'
+
 };
 	//'url' : 'your-database-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
