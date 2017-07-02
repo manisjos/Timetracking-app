203337fd4f362957a9688eb55e139bc7b67de10a
manisjos <manish.joshi.d@gmail.com>
2017-07-02 18:41:32 +0530

    Login and SignUp

---
+++ config/database.js
@@ -0,0 +1,8 @@
+//config/database.js
+module.exports = {
+
+    'url': 'mongodb://localhost/loginApp'
+};
+	//'url' : 'your-database-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+//mongodb://localhost/ImgUpDisp_MastekApp
