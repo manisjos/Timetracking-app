37ce3e22beb7bf952bf37734536bbd9e60bb7e92
manisjos <manish.joshi.d@gmail.com>
2017-07-04 23:29:43 +0530

    view changes

--- config/database.js
+++ config/database.js
@@ -3,7 +3,9 @@ module.exports = {
 
     // 'url': 'mongodb://localhost/loginApp'
         // 'url': 'mongodb://manisjosH:london@1@ds145312.mlab.com:45312/loginapp_manish'
-   'url' : 'mongodb://nodejsapi:nodejsapi@ds041516.mlab.com:41516/mynodejsapp'
+     'url' : 'mongodb://nodejsapi:nodejsapi@ds041516.mlab.com:41516/mynodejsapp'
+     // 'url' : 'mongodb://manisjosH:mLab$123@ds145312.mlab.com:45312/userdata'
+    
 };
 	//'url' : 'your-database-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
