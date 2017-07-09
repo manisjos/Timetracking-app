01bf5cb2e005e468b816cfc9f85b940fad7c4a35
manisjos <manish.joshi.d@gmail.com>
2017-07-09 11:44:48 +0530

    our own db

--- config/database.js
+++ config/database.js
@@ -3,8 +3,10 @@ module.exports = {
 
     // 'url': 'mongodb://localhost/loginApp'
         // 'url': 'mongodb://manisjosH:london@1@ds145312.mlab.com:45312/loginapp_manish'
-     'url' : 'mongodb://nodejsapi:nodejsapi@ds041516.mlab.com:41516/mynodejsapp'
+        //'url' : 'mongodb://nodejsapi:nodejsapi@ds041516.mlab.com:41516/mynodejsapp'
+     'url' : 'mongodb://timet_login:timet_login@ds151202.mlab.com:51202/timetrack_loginapp'
      // 'url' : 'mongodb://manisjosH:mLab$123@ds145312.mlab.com:45312/userdata'
+
     
 };
 	//'url' : 'your-database-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
